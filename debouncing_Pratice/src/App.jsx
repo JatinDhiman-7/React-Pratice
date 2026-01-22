fa4ae92e9a939f127f98f90ae12109eea4941e9f
unknown <jatin.careeer@gmail.com>
2026-01-22 18:49:50 +0530

    more pratice

--- debouncing_Pratice/src/App.jsx
+++ debouncing_Pratice/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Search() {
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('Search:', query);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [query])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <input value={query} onChange={e => setQuery(e.target.value)} placeholder="Search...." />
   )
 }
-
-export default App
+export default Search;
