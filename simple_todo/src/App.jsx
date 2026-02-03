e1b8a0b2f4556f8052b8c1d6da25122df3bad4f7
unknown <jatin.careeer@gmail.com>
2026-02-03 21:08:30 +0530

    update

--- simple_todo/src/App.jsx
+++ simple_todo/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use } from "react";
+import { useState } from "react";
+function todo() {
+  const [todo, settodo] = useState([]);
+  const [text, settext] = useState("")
 
-function App() {
-  const [count, setCount] = useState(0)
+  const addTodo = () => {
+    settodo([...todo, text]);
+    settext(" ")
+  }
+
+  const deleteTodo = (indexToDelete) => {
+    settodo(todo.filter((_, index) => index !== indexToDelete));
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {
+            todo.map((todo, index) => (
+              <li key={index}>{todo}
+                <button onClick={() => deleteTodo(index)}>Delete</button>
+              </li>
+            ))
+          }
+        </ul>
+        <input value={text} placeholder="enter task" onChange={(e) => settext(e.target.value)} />
+        <button onClick={addTodo} >Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
+export default todo;
