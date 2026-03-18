41c3e391a670db2b81ef0762752e16c44d91c129
unknown <jatin.careeer@gmail.com>
2026-03-18 17:21:47 +0530

    update

--- Pratice1/src/App.jsx
+++ Pratice1/src/App.jsx
@@ -1,23 +1,36 @@
-import Controlled from "./Controlled";
-import { useState } from "react";
-import Todo from "./todo";
-import Filter from "./Filter";
-import SagarCode from "./SagarCode";
-function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      {/* <h1>Counting:{count}</h1>
-      <button onClick={()=>setCount(count+1)}>Increment</button>
-      <button onClick={()=>setCount(count-1)}>Decrement</button>
-      <button onClick={()=>setCount(0)}>Reset</button> */}
-      {/* <Controlled /> */}
-      {/* <Todo/> */}
-      {/* <Filter/> */}
+// import Controlled from "./Controlled";
+// import { useState } from "react";
+// import Todo from "./todo";
+// import Filter from "./Filter";
+// import SagarCode from "./SagarCode";
+// function App() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       {/* <h1>Counting:{count}</h1>
+//       <button onClick={()=>setCount(count+1)}>Increment</button>
+//       <button onClick={()=>setCount(count-1)}>Decrement</button>
+//       <button onClick={()=>setCount(0)}>Reset</button> */}
+//       {/* <Controlled /> */}
+//       {/* <Todo/> */}
+//       {/* <Filter/> */}
 
-      <SagarCode />
-    </div>
+//       <SagarCode />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from 'react'
+const App = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setCount(count + 1)
+  }, [])
+  return (
+    <div>{count}</div>
   )
 }
 
-export default App;
+export default App
---
+++ Zustand/src/store/countStore.js
@@ -0,0 +1,8 @@
+import {create} from "zustand";
+
+export const useCount=create((set)=>({
+    count:0,
+    incCount:(num)=>{
+        set((state)=>({...state,count:state.count+num}));
+    }
+}))
